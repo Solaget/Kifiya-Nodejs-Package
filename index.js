635e6cb86015b95442ec1be583eee2e0801cd378
Solomon getnet <Solomongetnetofficial@gmail.com>
2024-12-09 03:10:41 +0300

    s

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-export { fakeApi } from "./fake-api";
+import axios from "axios";
+
+export class fakeApi {
+  constructor(secertKey) {
+    this.secertKey = secertKey;
+  }
+
+  async getFakeData({ size }) {
+    const response = await axios.get("https://fakestoreapi.com/users");
+    let data = await response.data;
+
+    if (size) {
+      data.length = size;
+    }
+
+    return { data, yourSecretKey: this.secertKey };
+  }
+}
